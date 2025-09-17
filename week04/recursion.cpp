632d0648b7d84ea560fd96202962a069295b0f10
Hazel Lindsey <clindsey7266@floridapoly.edu>
2025-09-17 14:49:23 +0000

    week5, class2

--- week04/recursion.cpp
+++ week04/recursion.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 unsigned int fact(unsigned int n)
@@ -29,11 +30,41 @@ unsigned int factLoop(unsigned int n)
     }
 }
 
+void sort(string& s, int high)
+{
+    if (high > 0)
+    {
+        int indexMax = 0;
+        char max = s[0];
+        for (int i = 1; i <= high; i++)
+        {
+            if (s[i] > max)
+            {
+                max = s[i];
+                indexMax = i;
+            }
+        }
+        s[indexMax] = s[high];
+        s[high] = max;
+        sort(s, high - 1);
+    }
+}
+
+void sort(string& s)
+{
+    sort(s, s.size() - 1);
+}
+
 int main()
 {
     unsigned int n = 0;
-    cout << "enter a number: ";
+    string in;
+    /*cout << "enter a number: ";
     cin >> n;
-    cout << "The factorial of " << n << " is " << factLoop(n) << endl;
-    return 0;
+    cout << "The factorial of " << n << " is " << factLoop(n) << endl;*/
+    cout << "Enter a word: ";
+    getline(cin, in, '\n');
+    cout << in << " sorted alphabetically is ";
+    sort(in);
+    cout << in << "." << endl;
 }
